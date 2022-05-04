ccb027018004a65af6f1ed40c1e63b1456859417
Karlyweatherstein <karlyweatherstein@gmail.com>
2022-05-03 23:48:59 -0400

    caught up

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@
 const Post = require('./Post');
 const User = require('./User');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 // create associations
 User.hasMany(Post, {
@@ -22,7 +23,7 @@ Post.belongsToMany(User, {
   through: Vote,
   as: 'voted_posts',
   foreignKey: 'post_id'
-})
+});
 
 Vote.belongsTo(User, {
   foreignKey: 'user_id'
@@ -40,4 +41,20 @@ Post.hasMany(Vote, {
   foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
