3226a357db61cb7d621211b857d1761aecd8448f
Karlyweatherstein <karlyweatherstein@gmail.com>
2022-05-04 11:38:44 -0400

    added seeds info

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,22 +1,10 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
+const { Post, User, Comment, Vote } = require('../models');
 
+// get all posts for homepage
 router.get('/', (req, res) => {
-  res.render('homepage', {
-    id: 1,
-    post_url: 'https://handlebarsjs.com/guide/',
-    title: 'Handlebars Docs',
-    created_at: new Date(),
-    vote_count: 10,
-    comments: [{}, {}],
-    user: {
-      username: 'test_user'
-    }
-  });
-});
-
-router.get('/', (req, res) => {
+  console.log('======================');
   Post.findAll({
     attributes: [
       'id',
@@ -41,10 +29,9 @@ router.get('/', (req, res) => {
     ]
   })
     .then(dbPostData => {
-      // pass a single post object into the homepage template
-      console.log(dbPostData[0]);
       const posts = dbPostData.map(post => post.get({ plain: true }));
-      res.render('homepage', { posts});
+
+      res.render('homepage', { posts });
     })
     .catch(err => {
       console.log(err);
