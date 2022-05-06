77d110642b15662ca40170f481c095ed2a3da74f
Karlyweatherstein <karlyweatherstein@gmail.com>
2022-05-05 22:54:14 -0400

    added parameters to login and out

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const path = require('path');
 const express = require('express');
-const exphbs = require('express-handlebars');
 const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const hbs = exphbs.create({});
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
@@ -21,6 +20,9 @@ const sess = {
 };
 
 app.use(session(sess));
+
+const hbs = exphbs.create({});
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
